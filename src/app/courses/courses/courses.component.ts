0ae46689a53833f3289145e92677c6a337c7cd3e
Bruno da Silva Barcelos <bbarcelos@ciandt.com>
2025-01-23 12:39:56 -0300

    feat: cria a pagina inicial de cursos disponiveis

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Course } from '../models/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent {
+  courses: Course[] = [{_id: '1', name: 'Angular', category: 'front-end'}];
+  displayedColumns: string[] = ['name', 'category'];
+
+}
