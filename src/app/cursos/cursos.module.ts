511ae40fc7b78e9826862487dc2744bca154e63d
Bruno da Silva Barcelos <bbarcelos@ciandt.com>
2025-03-24 19:25:28 -0300

    importando novos modulos e novas rotas

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -4,15 +4,19 @@ import { CommonModule } from '@angular/common';
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 import { SharedModule } from '../shared/shared.module';
+import { CursosFormComponent } from './cursos-form/cursos-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    CursosListaComponent
+    CursosListaComponent,
+    CursosFormComponent
   ],
   imports: [
     CommonModule,
     CursosRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class CursosModule { }
