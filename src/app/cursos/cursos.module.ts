e17e02cbc75ec68e4148dabaf8380d036466446b
Bruno da Silva Barcelos <bbarcelos@ciandt.com>
2025-03-24 15:15:47 -0300

    feat: adicionando configs e imports

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-  
     CursosListaComponent
   ],
   imports: [
     CommonModule,
-    CursosRoutingModule
+    CursosRoutingModule,
+    SharedModule
   ]
 })
 export class CursosModule { }
